2dcc7d51788006c764d21d2714086e90516a6453
Alex Steele <alexsteele110@gmail.com>
2018-03-16 10:48:58 -0400

    integrated sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
-import React from "react"
+import React from "react";
+import '../sass/main.scss';
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1 className="heading-primary">Discover Editor</h1>
+  </div>
+)
