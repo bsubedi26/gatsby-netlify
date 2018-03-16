18c9a41c387b4b1bb10e88cdde814445428df246
Alex Steele <alexsteele110@gmail.com>
2018-03-16 11:59:24 -0400

    blog post fix

---
+++ blog/test.md
@@ -0,0 +1,5 @@
+---
+title: "whatever"
+path: "/test"
+date: "2018-03-13"
+---
--- gatsby-node.js
+++ gatsby-node.js
@@ -21,13 +21,14 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       }
     }
   `).then(result => {
+    console.log(result);
     if (result.errors) {
       return Promise.reject(result.errors);
     }
 
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
-        path: node.frontmatter.path,
+        path: `/blog/${node.frontmatter.path}`,
         component: blogPostTemplate,
         context: {}, // additional data can be passed via context
       });
