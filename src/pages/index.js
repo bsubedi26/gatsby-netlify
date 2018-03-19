51dd01028a5020245af612cca1dfb98deab00aa2
Alex Steele <alexsteele110@gmail.com>
2018-03-19 11:28:14 -0400

    netlify identity login possible fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import '../sass/main.scss';
-import netlifyIdentity from 'netlify-identity-widget';
-
-window.netlifyIdentity = netlifyIdentity
-// You must run this once before trying to interact with the widget
-netlifyIdentity.init()
 
 export default () => (
   <div>
-    <h1 className="heading-primary">Discover Editor</h1>
-      <div data-netlify-identity-menu></div>
-      <div data-netlify-identity-button>Login with Netlify Identity</div>
+    <div className="home-container">
+      <h1 className="heading-primary">Discover Editor</h1>
+      <div data-netlify-identity-button></div>
+    </div>
   </div>
 )
