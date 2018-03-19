40b03e070ad2c07bc33eb7f74768cd47f14815d0
Alex Steele <alexsteele110@gmail.com>
2018-03-19 13:21:20 -0400

    update blog labeling to discover

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/discover`,
         name: 'markdown-pages'
       }
     },
